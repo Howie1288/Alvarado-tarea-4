3249d0d7d8578619d111c47a836630fe61f33897
¨Howard <programadorhaaj.2023@gmail.com>
2023-07-12 21:29:31 -0600

    Cracion de container, tipo de texto de los input y como agrega un nuevo contenedor

--- codigo.js
+++ codigo.js
@@ -2,3 +2,10 @@
 function addInput() {
     // Obtiene el elemento con el id 'input-container' y lo asigna a la variable inputContainer
     var inputContainer = document.getElementById('input-container');
+
+     // Establece el tipo de entrada del nuevo elemento como 'text'
+  newInput.type = 'text';
+
+   // Agrega el nuevo elemento de entrada como hijo del contenedor de entrada
+   inputContainer.appendChild(newInput);
+}
