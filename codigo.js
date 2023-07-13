5269ff2eee8be7b24eac592ea4f2a6f72b35e37c
¨Howard <programadorhaaj.2023@gmail.com>
2023-07-12 21:58:55 -0600

    Cracion de la funcion remover input

--- codigo.js
+++ codigo.js
@@ -9,3 +9,17 @@ function addInput() {
    // Agrega el nuevo elemento de entrada como hijo del contenedor de entrada
    inputContainer.appendChild(newInput);
 }
+// Definición de la función removeInput()
+function removeInput() {
+    // Obtiene el elemento con el id 'input-container' y lo asigna a la variable inputContainer
+    var inputContainer = document.getElementById('input-container');
+  
+    // Obtiene todos los elementos de entrada descendientes del contenedor de entrada y los asigna a la variable inputs
+    var inputs = inputContainer.getElementsByTagName('input');
+  
+    // Verifica si hay elementos de entrada presentes
+    if (inputs.length > 0) {
+      // Elimina el último elemento de entrada del contenedor
+      inputContainer.removeChild(inputs[inputs.length - 1]);
+    }
+}
