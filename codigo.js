e628e1f06c4669c23514094f21933d15cc6639bd
¨Howard <programadorhaaj.2023@gmail.com>
2023-07-13 07:16:07 -0600

    Definicion de la funcion Validar y verificar si hay error de entrada

--- codigo.js
+++ codigo.js
@@ -23,3 +23,42 @@ function removeInput() {
       inputContainer.removeChild(inputs[inputs.length - 1]);
     }
 }
+// Definición de la función validateForm(event)
+function validateForm(event) {
+  // Obtiene todos los elementos de entrada descendientes del elemento con el id 'dynamic-form' y los asigna a la variable inputs
+  var inputs = document.getElementById('dynamic-form').getElementsByTagName('input');
+
+  // Itera sobre cada elemento de entrada
+  for (var i = 0; i < inputs.length; i++) {
+    // Obtiene el elemento de entrada actual y lo asigna a la variable input
+    var input = inputs[i];
+
+    // Verifica si el valor del elemento de entrada está vacío
+    if (input.value === '') {
+      // Agrega la clase 'error' al elemento si está vacío
+      input.classList.add('error');
+    } else {
+      // Elimina la clase 'error' del elemento si no está vacío
+      input.classList.remove('error');
+    }
+  }
+  // Verifica si hay algún elemento de entrada con la clase 'error'
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].classList.contains('error')) {
+      // Evita que se envíe el formulario
+      event.preventDefault();
+
+      // Muestra una alerta al usuario para completar todos los campos obligatorios
+      alert('Por favor, complete todos los campos obligatorios.');
+
+      // Finaliza la función
+      return;
+    }
+  }
+
+  // Muestra una alerta al usuario de que el formulario se ha enviado con éxito
+  alert('¡Formulario enviado con éxito!');
+}
+
+// Agrega un evento 'submit' al elemento con el id 'dynamic-form', que llama a la función validateForm() cuando se envía el formulario
+document.getElementById('dynamic-form').addEventListener('submit', validateForm);
