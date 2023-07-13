031ad86f481217726ad1148edc16403f2c465d0d
¨Howard <programadorhaaj.2023@gmail.com>
2023-07-12 21:26:37 -0600

    Cracion del cidigo js

---
+++ codigo.js
@@ -0,0 +1,4 @@
+// Definición de la función addInput()
+function addInput() {
+    // Obtiene el elemento con el id 'input-container' y lo asigna a la variable inputContainer
+    var inputContainer = document.getElementById('input-container');
